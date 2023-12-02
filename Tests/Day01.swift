01df09226b99834c9264e4987858a8c1ffb1746e
DESKTOP-LQFE9AP\ryank <ryankmyers@gmail.com>
2023-12-02 01:04:37 -0500

    Continued cleanup

---
+++ Tests/Day01.swift
@@ -0,0 +1,25 @@
+import XCTest
+
+@testable import AdventOfCode
+
+// Make a copy of this file for every day to ensure the provided smoke tests
+// pass.
+final class Day00Tests: XCTestCase {
+  // Smoke test data provided in the challenge question
+  let testData = """
+    1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet
+    """
+
+  func testPart1() throws {
+    let challenge = Day01(data: testData)
+    XCTAssertEqual(String(describing: challenge.part1()), "142")
+  }
+
+  func testPart2() throws {
+    let challenge = Day01(data: testData)
+    XCTAssertEqual(String(describing: challenge.part2()), "32000")
+  }
+}
